4e98d1bc96c5a6a68e5b975c0bdc2104c550394e
BunnnyBin <dbqls8428@gmail.com>
2020-01-23 03:29:04 +0900

    8장

--- accounts/views.py
+++ accounts/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 from django.views.generic import CreateView
+from django.contrib.auth import login as auth_login
 
 @login_required  # 로그인된 상태가 보장됨
 def profile(request):
@@ -15,7 +16,11 @@ def signup(request):
         form = UserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
-            return redirect(settings.LOGIN_URL)
+            #로그인 처리
+            auth_login(request, user)
+            # 회원가입과 next
+            next_url = request.GET.get('next') or 'profile'  # next 인자 가져오기 인자 없으면 profile
+            return redirect(next_url)
     else:
         form = UserCreationForm()
     return render(request, 'accounts/signup.html', {
