556fb7ddc01be5212c55d4eea06f98cad3c16618
BunnnyBin <dbqls8428@gmail.com>
2020-01-23 01:49:32 +0900

    3장

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+@login_required  # 로그인된 상태가 보장됨
+def profile(request):
+    return render(request, 'accounts/profile.html')
