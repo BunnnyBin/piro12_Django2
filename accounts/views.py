1fb27aa6c8f92dc6a30f591177205a3e678cb8d5
BunnnyBin <dbqls8428@gmail.com>
2020-01-23 02:20:27 +0900

    5장

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,28 @@
-from django.shortcuts import render
+from django.conf import settings
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
+from django.views.generic import CreateView
 
 @login_required  # 로그인된 상태가 보장됨
 def profile(request):
     return render(request, 'accounts/profile.html')
+
+'''
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            return redirect(settings.LOGIN_URL)
+    else:
+        form = UserCreationForm()
+    return render(request, 'accounts/signup.html', {
+        'form': form,
+    })
+'''
+
+signup = CreateView.as_view(model=User, form_class=UserCreationForm,
+                            success_url='settings.LOGIN_URL',
+                            template_name='accounts/signup.html')
